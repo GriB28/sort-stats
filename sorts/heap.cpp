3d7b79955dbafd57fa52d1e62d5e00698a2cca38
|GriB28| <gp.off.dev@gmail.com>
2025-09-30 16:49:25 +0300

    .cpp for logarithmic algorithms

---
+++ sorts/heap.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include "../lib/lib.h"
+using std::fstream;
+
+
+constexpr size_t MAX_LENGTH = 100000000;
+static int array[MAX_LENGTH];
+int main() {
+    fstream data("data/input.csv", std::ios::in);
+    fstream result("data/heap.csv", std::ios::app);
+
+    size_t length;
+    int max, min;
+    data >> length >> max >> min;
+    utils::randomize_array(min, max, length, array);
+
+    long long start_time_stamp = utils::get_time();
+    sort::heap(array, length);
+
+    result << (utils::get_time() - start_time_stamp) << ',' << length << '\n';
+    return 0;
+}
