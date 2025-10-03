fc0bd0251ad3d558f489970d96f74ab8e3861ed1
|GriB28| <gp.off.dev@gmail.com>
2025-10-03 17:57:36 +0300

    bubble sort update

--- sorts/bubble.cpp
+++ sorts/bubble.cpp
@@ -19,26 +19,27 @@ int main() {
     auto *result = new fstream("data/" + output_file + ".csv", std::ios::out);
     utils::prepare_result_file(result, graph_name, Ox, Oy);
 
-    for (size_t i = 0; i < iterations; i++) {
+    for (size_t iteration = 0; iteration < iterations; iteration++) {
+        size_t local_length = length + iteration;
         switch (array_type) {
             case 0:
-                utils::random_array(min, max, length, array);
+                utils::random_array(min, max, local_length, array);
                 break;
             case 1:
-                utils::ascending_array(min, max, length, array);
+                utils::ascending_array(min, max, local_length, array);
                 break;
             case 2:
-                utils::descending_array(min, max, length, array);
+                utils::descending_array(min, max, local_length, array);
                 break;
             default:
                 break;
         }
 
         const long long start_time_stamp = utils::get_time();
-        sort::bubble(array, length);
+        sort::bubble(array, local_length);
         const long long stop_time_stamp = utils::get_time();
 
-        utils::add_result(length, stop_time_stamp - start_time_stamp);
+        utils::add_result(local_length, stop_time_stamp - start_time_stamp);
     }
     delete result;
     return 0;
